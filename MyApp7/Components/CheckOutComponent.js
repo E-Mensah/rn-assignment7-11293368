86462f53a0709dd79d6b058fc88e0174398abe65
Mensah <mensaherskine@gmail.com>
2024-07-10 02:04:36 +0000

    Setting up project with required files and folders and setting up temporal local  data storage

---
+++ MyApp7/Components/CheckOutComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const CheckOutComponent = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default CheckOutComponView
